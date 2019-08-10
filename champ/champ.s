3d8927443d120e824cb7c28fb5d9ba668aac1cee
Alexander <alexandercarlson99@gmail.com>
2019-08-10 09:58:11 -0700

    Moved champ.  Changed comment_char

---
+++ champ/champ.s
@@ -0,0 +1,137 @@
+.name "boi"
+.comment "yee"
+
+start:
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	fork %:start
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	st r1, 6
+	live %69
+	lfork %1000
+	lfork %:start
+	or r2, r2, r2
+	zjmp %511
